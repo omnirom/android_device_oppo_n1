300fb5986e94daffee2334a55926b0093df6cb8c
maxwen <max.weninger@gmail.com>
2013-11-29 02:08:01 +0100

    n1: force enable video snapshot support
    Change-Id: I08bca2574a7bf6ea0a0388e3b74a5130a31ed0a2

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -100,6 +100,9 @@ static char * camera_fixup_getparams(int id, const char * settings)
     android::CameraParameters params;
     params.unflatten(android::String8(settings));
 
+    // added video snapshot supported
+    params.set(android::CameraParameters::KEY_VIDEO_SNAPSHOT_SUPPORTED, "true");
+
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
 
